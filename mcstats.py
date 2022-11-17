2352be2ee8f8421374f5ad81a3f8d21a44a8e4d5
chfour <chfourchfour@protonmail.com>
2022-11-17 21:25:37 +0100

    count idle time + cleanups

--- mcstats.py
+++ mcstats.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 import glob, sys, os.path, re
-from datetime import datetime, date, time, timedelta
+from datetime import datetime, timedelta
 
 root_dir = sys.argv[1]
 stats = {
     "server": {
         "last": None,
-        "total": timedelta()
+        "total": timedelta(),
+        "idle": timedelta(),
+        "idlestart": None
     },
     "players": {}
 }
 
 for logfile in sorted(glob.iglob("*.log", root_dir=root_dir), key=lambda n: [int(i) for i in n[:-4].split("-")]):
     filedate = datetime.strptime(logfile[:logfile.rindex("-")], "%Y-%m-%d")
-    
+
     logfile = os.path.join(root_dir, logfile)
     print(f"  opening {logfile} ({filedate.date()})", file=sys.stderr)
 
@@ -29,18 +31,21 @@ for logfile in sorted(glob.iglob("*.log", root_dir=root_dir), key=lambda n: [int
 
                 # start
                 if line.startswith("Starting minecraft server"):
-                    stats["server"]["last"] = msgtime
+                    stats["server"]["idlestart"] = stats["server"]["last"] = msgtime
                     print(f"! {msgtime} : start", file=sys.stderr)
+                    print("* idle: started", file=sys.stderr)
 
                 # stop
                 elif line == "Closing Server":
                     stats["server"]["total"] += msgtime - stats["server"]["last"]
+                    stats["server"]["idle"] += msgtime - stats["server"]["idlestart"]
                     print(f"! {msgtime} : stop -> running count is {stats['server']['total']}", file=sys.stderr)
+                    print(f"* dump idle: server stop -> running count is {stats['server']['idle']}", file=sys.stderr)
 
                 # joined
                 elif re.match(r"^[a-zA-Z0-9_]* joined the game$", line):
                     player = line[:line.index(" ")]
-                    print(f"! {msgtime} : joined:  '{player: <16}'")
+                    print(f"! {msgtime} : joined:  '{player: <16}'", file=sys.stderr)
                     if player not in stats["players"]: stats["players"][player] = {
                         "last": None, "playtime": timedelta(),
                         "deaths": 0, "commands": 0,
@@ -48,6 +53,9 @@ for logfile in sorted(glob.iglob("*.log", root_dir=root_dir), key=lambda n: [int
                     }
                     stats["players"][player]["last"] = msgtime
                     stats["players"][player]["online"] = True
+                    if len(list(filter(lambda p: p["online"], stats["players"].values()))) < 2:
+                        stats["server"]["idle"] += msgtime - stats["server"]["idlestart"]
+                        print(f"* no longer idle: first player joined -> running count is {stats['server']['idle']}", file=sys.stderr)
 
                 # left
                 elif re.match(r"^[a-zA-Z0-9_]* lost connection: .*$", line):
@@ -55,26 +63,30 @@ for logfile in sorted(glob.iglob("*.log", root_dir=root_dir), key=lambda n: [int
                     reason = line[line.index(" "):][18:]
                     stats["players"][player]["playtime"] += msgtime - stats["players"][player]["last"]
                     stats["players"][player]["online"] = False
-                    print(f"! {msgtime} : left:    '{player: <16}', '{reason}' -> running count is {stats['players'][player]['playtime']}")
+                    print(f"! {msgtime} : left:    '{player: <16}', '{reason}' -> running count is {stats['players'][player]['playtime']}", file=sys.stderr)
+                    if not any([p["online"] for p in stats["players"].values()]):
+                        stats["server"]["idlestart"] = msgtime
+                        print("* idle: no online players", file=sys.stderr)
+                        
 
                 # death
                 elif re.match(r"^[a-zA-Z0-9_]* (was|drowned|experienced|blew|hit|fell|went|walked|burned|discovered|froze|starved|suffocated|didn't|withered|died).*$", line):
                     player = line[:line.index(" ")]
                     reason = line[line.index(" ")+1:]
-                    print(f"! {msgtime} : death:   '{player: <16}', '{reason}'")
+                    print(f"! {msgtime} : death:   '{player: <16}', '{reason}'", file=sys.stderr)
                     stats["players"][player]["deaths"] += 1
 
                 # command
                 elif re.match(r"^[a-zA-Z0-9_]* issued server command: .*$", line):
                     player = line[:line.index(" ")]
                     command = line[line.index(": ")+2:]
-                    print(f"! {msgtime} : command: '{player: <16}', '{command}'")
+                    print(f"! {msgtime} : command: '{player: <16}', '{command}'", file=sys.stderr)
                     stats["players"][player]["commands"] += 1
 
                 elif re.match(r"^[a-zA-Z0-9_]* has made the advancement \[.*\]$", line):
                     player = line[:line.index(" ")]
                     advname = line[line.index(" [")+2:-1]
-                    print(f"! {msgtime} : adv:     '{player: <16}', '{advname}'")
+                    print(f"! {msgtime} : adv:     '{player: <16}', '{advname}'", file=sys.stderr)
                     stats["players"][player]["advancements"] += 1
 
             # chatmsg
@@ -82,7 +94,7 @@ for logfile in sorted(glob.iglob("*.log", root_dir=root_dir), key=lambda n: [int
                 line = line[line.index(": <")+3:]
                 player = line[:line.index("> ")]
                 content = line[line.index("> ")+2:]
-                print(f"! {msgtime} : chatmsg: '{player: <16}', '{content}'")
+                print(f"! {msgtime} : chatmsg: '{player: <16}', '{content}'", file=sys.stderr)
                 stats["players"][player]["messages"] += 1
                 
 
